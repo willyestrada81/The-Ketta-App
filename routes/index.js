51ef1fb0fa199613e459aebd6562330a55ea13f8
williamestrada <willy.estrada81@gmail.com>
2019-09-24 01:52:18 -0400

    Show Page Done, restaurant page route created

--- routes/index.js
+++ routes/index.js
@@ -1,67 +1,85 @@
+require('dotenv').config()
 const express = require("express")
 const router = express.Router();
 const GoogleAPI = require('../config/keys').GoogleAPI;
 const axios = require('axios');
+var NodeGeocoder = require('node-geocoder');
 
-//Landing Page
+var options = {
+    provider: 'google',
+    httpAdapter: 'https',
+    apiKey: process.env.GEOCODER_API_KEY,
+    formatter: null
+};
+
+var geocoder = NodeGeocoder(options);
+// Define escapeRegex function for search feature
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
+
+// Landing Page
 router.get('/', (req, res) => {
     res.render('landing')
 })
 
 
-
-
 router.post('/', (req, res) => {
   
-    let queryParams = {
-        input: req.body.restaurantName + '%20' + req.body.city,
-        inputtype: 'textquery',
-        fields: 'place_id,photos,formatted_address,name,rating,opening_hours,geometry,icon,user_ratings_total',
-        APIkey: GoogleAPI
-    }
-    let requestURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${queryParams.input}&inputtype=${queryParams.inputtype}&fields=${queryParams.fields}&key=${queryParams.APIkey}`
-    
-    let responseData = []
-    let photoURL = ''
- 
-    axios.get(requestURL)
-        .then(function (response) {
-            
-            let candidates = response.data.candidates;
+let location = escapeRegex(req.body.location);
+    geocoder.geocode(location)
+        .then(function (data) {
 
-            for(var i = 0; i < candidates.length; i++){
-                responseData[i] = {
-                    formatted_address: candidates[i].formatted_address,
-                    geometry: candidates[i].geometry.location,
-                    name: candidates[i].name,
-                    opening_hours: candidates[i].opening_hours,
-                    rating: candidates[i].rating,
-                    photos: candidates[i].photos[i],
-                    user_ratings_total: candidates[i].user_ratings_total
-                }
-                
+            console.log(data);
+            let lat = data[0].latitude;
+            let lng = data[0].longitude
+            console.log(`${lat} + ${lng}`)
+             
+            let queryParams = {
+                location: { lat: lat, lng: lng },
+                radius: '8047',
+                type: 'restaurant',
+                keyword: escapeRegex(req.body.keyword),
+                APIkey: process.env.GoogleAPI
             }
 
-        }).then(() => {
-            let photoReference = responseData[0].photos.photo_reference; 
-            photoURL = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=1100&photoreference=${photoReference}&key=${queryParams.APIkey}`;
-         
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-        .finally(function () {
-            console.log(responseData)
-            // console.log(responseData);
-            res.render("index", {
-                restaurantData: responseData,
-                restaurantPhoto: photoURL
-                
-            })
-            
-        });
+             let requestURL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${queryParams.location.lat},${queryParams.location.lng}&radius=${queryParams.radius}&type=${queryParams.type}&keyword=${queryParams.keyword}&key=${queryParams.APIkey}`
     
+             let responseData
+ 
+             axios.get(requestURL)
+             .then(function (response) {
+            
+            responseData = response.data.results;
+                 let totalResults = responseData.length;
+
+            res.render("show", {
+                restaurantData: responseData,
+                totalResults: totalResults,
+                location: location
+            })
+
+           
+              })
+                 .catch(function (error) {
+                    // handle error
+                     console.log(error);
+                 })
+                .finally(function () {
+                console.log(responseData)
+                // // console.log(responseData);
+                // res.render("index", {
+                //     restaurantData: responseData
+                    
+                // })
+                
+        });
+    })
+})
+
+router.get('/restaurant/:id', (req, res) => {
+    res.send('<h1>Welcome to the show page</h1>')
 })
 
 module.exports = router;
