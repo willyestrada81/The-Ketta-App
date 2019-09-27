dedebbc446b91d06acef595734ecd8d767656479
williamestrada <willy.estrada81@gmail.com>
2019-09-27 13:24:00 -0400

    MongoDB Variable updated in .env file

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require("express")
 const router = express.Router();
-const GoogleAPI = require('../config/keys').GoogleAPI;
+const GoogleAPI = process.env.GoogleAPI;
 const axios = require('axios');
 var NodeGeocoder = require('node-geocoder');
 
