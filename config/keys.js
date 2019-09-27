42861f2fa7e246914db26ca7e9905c45a6bccede
William Estrada <34007144+willyestrada81@users.noreply.github.com>
2019-09-27 12:51:48 -0400

    Delete keys.js

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MongoURI: 'mongodb+srv://william_admin:Kenlm8281!@cluster0-ekbve.mongodb.net/test?retryWrites=true&w=majority',
-    GoogleAPI: 'AIzaSyDmJONftde81AKG-bNjXdLvbWReOPHQyc0'
-}
